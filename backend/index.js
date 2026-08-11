1ab811830a1dbc01d8e663d581028034c9aa57c6
Pankaj Mehta <pankajmehta501@gmail.com>
2026-08-11 18:30:34 +0530

    Disable ETags to prevent 304 responses - always return full response

--- backend/index.js
+++ backend/index.js
@@ -39,12 +39,15 @@ app.use(cors({
   credentials: true
 }));
 
+// Disable ETags globally
+app.disable('etag');
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Disable caching on API responses
 app.use('/api', (req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
   res.set('Pragma', 'no-cache');
   res.set('Expires', '0');
   next();
